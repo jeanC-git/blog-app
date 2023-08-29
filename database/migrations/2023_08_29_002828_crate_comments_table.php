82767cf59ab6f1e454ce9aa876c1d76836b27e2b
JC <jc@MacBook-Pro-de-JC.local>
2023-08-28 19:29:33 -0500

    comments migration created

---
+++ database/migrations/2023_08_29_002828_crate_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('body');
+
+
+            $table->foreignId('author_id')->nullable()->constrained('users');
+            $table->foreignId('parent_comment_id')->nullable()->constrained('comments');
+
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
